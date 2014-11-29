f0cc02e0ed62076d4bc30c1600b0e9ef645bb403
Joachim Metz <joachim.metz@gmail.com>
2014-11-29 09:14:22 +0100

    Code clean up.

--- libqcow/libqcow_support.h
+++ libqcow/libqcow_support.h
@@ -69,11 +69,13 @@ int libqcow_check_file_signature(
      libcerror_error_t **error );
 
 #if defined( HAVE_WIDE_CHARACTER_TYPE )
+
 LIBQCOW_EXTERN \
 int libqcow_check_file_signature_wide(
      const wchar_t *filename,
      libcerror_error_t **error );
-#endif
+
+#endif /* defined( HAVE_WIDE_CHARACTER_TYPE ) */
 
 LIBQCOW_EXTERN \
 int libqcow_check_file_signature_file_io_handle(
