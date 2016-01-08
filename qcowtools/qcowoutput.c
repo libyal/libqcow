2fedfcf41fae6c9dbe79a7412318b88cd93e771e
Joachim Metz <joachim.metz@gmail.com>
2016-01-08 06:27:34 +0100

    Applied updates.

--- qcowtools/qcowoutput.c
+++ qcowtools/qcowoutput.c
@@ -1,7 +1,7 @@
 /*
  * Common output functions for the qcowtools
  *
- * Copyright (C) 2010-2015, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2016, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
@@ -46,7 +46,7 @@ void qcowoutput_copyright_fprint(
 	 */
 	fprintf(
 	 stream,
-	 _( "Copyright (C) 2010-2015, %s.\n" ),
+	 _( "Copyright (C) 2010-2016, %s.\n" ),
 	 _( "Joachim Metz" ) );
 
 	fprintf(
