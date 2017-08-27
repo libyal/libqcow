bd728bec08f909732abce1c1a37f9dbb96e6aa37
Joachim Metz <joachim.metz@gmail.com>
2017-08-27 10:06:54 +0200

    Worked on tests

--- tests/qcow_test_io_handle.c
+++ tests/qcow_test_io_handle.c
@@ -35,7 +35,7 @@
 
 #include "../libqcow/libqcow_io_handle.h"
 
-#if defined( __GNUC__ )
+#if defined( __GNUC__ ) && !defined( LIBQCOW_DLL_IMPORT )
 
 /* Tests the libqcow_io_handle_initialize function
  * Returns 1 if successful or 0 if not
@@ -270,7 +270,7 @@ on_error:
 	return( 0 );
 }
 
-#endif /* defined( __GNUC__ ) */
+#endif /* defined( __GNUC__ ) && !defined( LIBQCOW_DLL_IMPORT ) */
 
 /* The main program
  */
@@ -287,7 +287,7 @@ int main(
 	QCOW_TEST_UNREFERENCED_PARAMETER( argc )
 	QCOW_TEST_UNREFERENCED_PARAMETER( argv )
 
-#if defined( __GNUC__ )
+#if defined( __GNUC__ ) && !defined( LIBQCOW_DLL_IMPORT )
 
 	QCOW_TEST_RUN(
 	 "libqcow_io_handle_initialize",
@@ -305,7 +305,7 @@ int main(
 
 	/* TODO: add tests for libqcow_io_handle_read_cluster_block */
 
-#endif /* defined( __GNUC__ ) */
+#endif /* defined( __GNUC__ ) && !defined( LIBQCOW_DLL_IMPORT ) */
 
 	return( EXIT_SUCCESS );
 
