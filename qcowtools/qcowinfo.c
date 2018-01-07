e1df685e5b10b1528c3c659fdc7e51ed51c48900
Joachim Metz <joachim.metz@gmail.com>
2018-01-07 14:09:13 +0100

    Applied updates

--- qcowtools/qcowinfo.c
+++ qcowtools/qcowinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a QEMU Copy-On-Write (QCOW) image file
  *
- * Copyright (C) 2010-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
