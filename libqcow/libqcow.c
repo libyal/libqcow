8078cbf6e7f0dd5b742d2722458627c8841d66fc
Joachim Metz <joachim.metz@gmail.com>
2022-02-16 05:23:50 +0100

    Applied updates

--- libqcow/libqcow.c
+++ libqcow/libqcow.c
@@ -1,7 +1,7 @@
 /*
  * Library to access the QEMU Copy-On-Write (QCOW) image file format
  *
- * Copyright (C) 2010-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
--- qcowtools/qcowinfo.c
+++ qcowtools/qcowinfo.c
@@ -1,7 +1,7 @@
 /*
  * Shows information obtained from a QEMU Copy-On-Write (QCOW) image file
  *
- * Copyright (C) 2010-2021, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2010-2022, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
