036907d9629d70f413bc0e0df101a1c568124419
Joachim Metz <joachim.metz@gmail.com>
2024-05-09 06:15:02 +0200

    Changes for deployment

--- pyqcow/pyqcow_file.c
+++ pyqcow/pyqcow_file.c
@@ -91,14 +91,14 @@ PyMethodDef pyqcow_file_object_methods[] = {
 	{ "read_buffer",
 	  (PyCFunction) pyqcow_file_read_buffer,
 	  METH_VARARGS | METH_KEYWORDS,
-	  "read_buffer(size) -> Binary string\n"
+	  "read_buffer(size)-> Bytes\n"
 	  "\n"
 	  "Reads a buffer of data." },
 
 	{ "read_buffer_at_offset",
 	  (PyCFunction) pyqcow_file_read_buffer_at_offset,
 	  METH_VARARGS | METH_KEYWORDS,
-	  "read_buffer_at_offset(size, offset) -> Binary string\n"
+	  "read_buffer_at_offset(size, offset)-> Bytes\n"
 	  "\n"
 	  "Reads a buffer of data at a specific offset." },
 
@@ -121,7 +121,7 @@ PyMethodDef pyqcow_file_object_methods[] = {
 	{ "read",
 	  (PyCFunction) pyqcow_file_read_buffer,
 	  METH_VARARGS | METH_KEYWORDS,
-	  "read(size) -> Binary string\n"
+	  "read(size)-> Bytes\n"
 	  "\n"
 	  "Reads a buffer of data." },
 
