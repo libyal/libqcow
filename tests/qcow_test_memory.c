86055af30f143cf2859cc591f0c60657cfa311a9
Joachim Metz <joachim.metz@gmail.com>
2026-06-15 08:17:53 +0200

    Replaced test scripts with autotest

--- tests/qcow_test_memory.c
+++ tests/qcow_test_memory.c
@@ -35,15 +35,8 @@
 
 #if defined( HAVE_QCOW_TEST_MEMORY )
 
-static void *(*qcow_test_real_malloc)(size_t)                       = NULL;
-static void *(*qcow_test_real_memcpy)(void *, const void *, size_t) = NULL;
-static void *(*qcow_test_real_memset)(void *, int, size_t)          = NULL;
-static void *(*qcow_test_real_realloc)(void *, size_t)              = NULL;
-
-int qcow_test_malloc_attempts_before_fail                           = -1;
-int qcow_test_memcpy_attempts_before_fail                           = -1;
-int qcow_test_memset_attempts_before_fail                           = -1;
-int qcow_test_realloc_attempts_before_fail                          = -1;
+static void *(*qcow_test_real_malloc)(size_t) = NULL;
+int qcow_test_malloc_attempts_before_fail     = -1;
 
 /* Custom malloc for testing memory error cases
  * Note this function might fail if compiled with optimation
@@ -59,6 +52,11 @@ void *malloc(
 		qcow_test_real_malloc = dlsym(
 		                         RTLD_NEXT,
 		                         "malloc" );
+
+		if( qcow_test_real_malloc == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( qcow_test_malloc_attempts_before_fail == 0 )
 	{
@@ -76,6 +74,9 @@ void *malloc(
 	return( ptr );
 }
 
+static void *(*qcow_test_real_memcpy)(void *, const void *, size_t) = NULL;
+int qcow_test_memcpy_attempts_before_fail                           = -1;
+
 /* Custom memcpy for testing memory error cases
  * Note this function might fail if compiled with optimation and as a shared libary
  * Returns a pointer to newly allocated data or NULL
@@ -90,6 +91,11 @@ void *memcpy(
 		qcow_test_real_memcpy = dlsym(
 		                         RTLD_NEXT,
 		                         "memcpy" );
+
+		if( qcow_test_real_memcpy == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( qcow_test_memcpy_attempts_before_fail == 0 )
 	{
@@ -109,6 +115,9 @@ void *memcpy(
 	return( destination );
 }
 
+static void *(*qcow_test_real_memset)(void *, int, size_t) = NULL;
+int qcow_test_memset_attempts_before_fail                  = -1;
+
 /* Custom memset for testing memory error cases
  * Note this function might fail if compiled with optimation and as a shared libary
  * Returns a pointer to newly allocated data or NULL
@@ -123,6 +132,11 @@ void *memset(
 		qcow_test_real_memset = dlsym(
 		                         RTLD_NEXT,
 		                         "memset" );
+
+		if( qcow_test_real_memset == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( qcow_test_memset_attempts_before_fail == 0 )
 	{
@@ -142,6 +156,9 @@ void *memset(
 	return( ptr );
 }
 
+static void *(*qcow_test_real_realloc)(void *, size_t) = NULL;
+int qcow_test_realloc_attempts_before_fail             = -1;
+
 /* Custom realloc for testing memory error cases
  * Note this function might fail if compiled with optimation
  * Returns a pointer to reallocated data or NULL
@@ -155,6 +172,11 @@ void *realloc(
 		qcow_test_real_realloc = dlsym(
 		                          RTLD_NEXT,
 		                          "realloc" );
+
+		if( qcow_test_real_realloc == NULL )
+		{
+			return( NULL );
+		}
 	}
 	if( qcow_test_realloc_attempts_before_fail == 0 )
 	{
